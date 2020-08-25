3da12a4130e17b037740f4845de99ef8e3c14e9a
Serg <zavasserejaya@mail.ru>
2020-08-25 18:38:20 +0300

    Выполнена 5 домашка

---
+++ src/main/java/ru/geekbrains/main/site/at/block/HeaderCoursesBlock.java
@@ -0,0 +1,51 @@
+package ru.geekbrains.main.site.at.block;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import ru.geekbrains.main.site.at.BasePage;
+import ru.geekbrains.main.site.at.page.ContentPage;
+
+public class HeaderCoursesBlock extends BasePage {
+    @FindBy(css = "[id=\"prof-link\"]")
+    private WebElement professions;
+
+    @FindBy(css = "[id=\"free-link\"]")
+    private WebElement freeIntensives;
+
+    @FindBy(css = "[id=\"cour-link\"]")
+    private WebElement courses;
+
+    @FindBy(css = "[class=\"text-dark-dk\"][target=\"_blank\"]")
+    private WebElement companies;
+
+    public HeaderCoursesBlock(WebDriver driver) {
+        super(driver);
+    }
+
+    public ContentPage clickButton(String nameButton) {
+        switch (nameButton) {
+            case "Профессии": {
+                this.professions.click();
+                break;
+            }
+            case "Бесплатные интенсивы": {
+                this.freeIntensives.click();
+                break;
+            }
+            case "Курсы": {
+                this.courses.click();
+                break;
+            }
+            case "Компаниям": {
+                this.companies.click();
+                break;
+            }
+
+            default: {
+                throw new RuntimeException("Кнопки: " + nameButton + " нет на странице!");
+            }
+        }
+        return new ContentPage(driver);
+    }
+}
---
+++ src/main/java/ru/geekbrains/main/site/at/block/SampleBlock.java
@@ -0,0 +1,25 @@
+package ru.geekbrains.main.site.at.block;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import ru.geekbrains.main.site.at.BasePage;
+import ru.geekbrains.main.site.at.page.ContentPage;
+
+public class SampleBlock extends BasePage {
+
+    @FindBy(css = "[id=\"courses-tab\"]")
+    private WebElement fullSample;
+
+    public SampleBlock(WebDriver driver) {
+        super(driver);
+    }
+    public ContentPage searchContent(String nameContent){
+        webDriverWait.until(ExpectedConditions.textToBePresentInElement(fullSample,nameContent));
+        return new ContentPage(driver);
+
+
+    }
+
+}
